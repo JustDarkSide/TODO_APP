fc241186efe20b23d3df5bd6df76de1931475940
Kacper <krzysztofzajac975@gmail.com>
2026-03-29 21:02:41 +0200

    Added basic button functionality. Added hidden task adding window

---
+++ JS/buttons-and-switchers.js
@@ -0,0 +1,83 @@
+let allSwitch = document.querySelector(".task-selector__item__button__all");
+
+let activeSwitch = document.querySelector(
+	".task-selector__item__button__active",
+);
+let completedSwitch = document.querySelector(
+	".task-selector__item__button__completed",
+);
+let allSwitches = [allSwitch, activeSwitch, completedSwitch];
+
+let addButton = document.querySelector(".task-manager.add-task");
+
+let addTaskWindow = document.querySelector(".add-task__window");
+
+let shadow = document.querySelector(".shadow");
+
+const handleSwitch = (currentSwitch) => {
+	currentSwitch.classList.toggle("active");
+	currentSwitch.firstElementChild.classList.toggle("moved");
+};
+
+const checkAcivationPermission = (switchList, currentSwitch) => {
+	let allow_for_action = true;
+	switchList.forEach((element) => {
+		if (element.classList.contains("active") && element != currentSwitch) {
+			allow_for_action = false;
+		}
+	});
+	return allow_for_action;
+};
+const blockOrUnblock = (allSwitches, currentSwitch) => {
+	if (currentSwitch.classList.contains("active")) {
+		allSwitches.forEach((element) => {
+			element.classList.remove("disabled");
+		});
+	} else {
+		allSwitches.forEach((element) => {
+			if (element != currentSwitch) {
+				element.classList.add("disabled");
+			}
+		});
+	}
+};
+
+allSwitch.addEventListener("click", (e) => {
+	permission = checkAcivationPermission(allSwitches, e.currentTarget);
+	if (permission) {
+		blockOrUnblock(allSwitches, e.currentTarget);
+		handleSwitch(e.currentTarget);
+	}
+});
+
+activeSwitch.addEventListener("click", (e) => {
+	permission = checkAcivationPermission(allSwitches, e.currentTarget);
+	if (permission) {
+		blockOrUnblock(allSwitches, e.currentTarget);
+		handleSwitch(e.currentTarget);
+	}
+});
+
+completedSwitch.addEventListener("click", (e) => {
+	permission = checkAcivationPermission(allSwitches, e.currentTarget);
+	if (permission) {
+		blockOrUnblock(allSwitches, e.currentTarget);
+		handleSwitch(e.currentTarget);
+	}
+});
+
+addButton.addEventListener("click", () => {
+	addTaskWindow.classList.toggle("add-task__window__show");
+	shadow.style.zIndex = 1;
+	shadow.style.opacity = 1;
+});
+
+shadow.addEventListener("click", () => {
+	if (addTaskWindow.classList.contains("add-task__window__show")) {
+		shadow.style.opacity = 0;
+        addTaskWindow.classList.remove("add-task__window__show")
+		setTimeout(() => {
+			shadow.style.zIndex = -1;
+		}, 300);
+	}
+});
